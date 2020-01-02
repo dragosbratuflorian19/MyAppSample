5f082b79811710a297de921e4ad8b6656e45f790
Dragos Bratu <dragosbratuflorian19@gmail.com>
2020-01-03 00:44:57 +0200

    Initial commit

---
+++ Git_notes.py
@@ -0,0 +1,9 @@
+Git is a version control system for tracking changes in files
+Revert changes
+Local and remote repos
+
+$ git init # Initialize local Git Repository
+$ git add <file> # Add Files to Index
+$ git status # Check status of the working tree
+$ git commit # Check status
+$ git
