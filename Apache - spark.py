26ef02662d86f77dd06db713f967bb99f50ce9c9
Dragos Bratu <dragosbratuflorian19@gmail.com>
2020-01-04 23:55:54 +0200

    Apache modified

--- Apache - spark.py
+++ Apache - spark.py
@@ -1,13 +1,6 @@
-Apache Spark is a fast (in memory) data processing engine which allows to:
-- Data classification through Spark machine learning
-- Streaming data through source via Spark Streaming
-- Querying data in real time throgh Spark SQL
+What is Apache Spark:	Apache Spark is a fast(in memory) data processing engine which allows to:
+    - Data classification through Spark machine learning
+    - Streaming data through source via Spark Streaming
+    - Querying data in real time throgh Spark SQL
 
 Spark is built on the top of Scala programming language, which runs on the Java Virtual Machine
-
-Spark runs computations in memory
-
-sc = SparkContext ("local[3]","word count") - Is the entrypoint to the spark core
-spark app is named "word count"
-local[3] - local box that can use up to 3 cores of our cpu
-lines = sc.textFile("text file") - this is an rdd
