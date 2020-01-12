c79818cde2404093ceb730db81557fd199b85ed5
Dragos Bratu <dragosbratuflorian19@gmail.com>
2020-01-12 02:30:29 +0200

    Lnear

--- ML_notes.py
+++ ML_notes.py
@@ -91,14 +91,19 @@ def best_fit_slope_and_intersect(xs, ys):
 
 ###################################################
 # Calculate the error
+
+
 def squared_error(ys_orig, ys_line):
     return sum((ys_line - ys_orig)**2)
+
 
 def coefficient_of_determination(ys_orig, ys_line):
     y_mean_line = [mean(ys_orig) for y in ys_orig]
     squared_error_regr = squared_error(ys_orig, ys_line)
     squared_error_y_mean = squared_error(ys_orig, y_mean_line)
     return 1 - (squared_error_regr / squared_error_y_mean)
+
+
 ###################################################
 # making the splitting between for training and data for testing
 from sklearn import preprocessing, model_selection, neighbors
