865603455c49bd1fafd19f18efca4c28c9aeb0d6
Dragos Bratu <dragosbratuflorian19@gmail.com>
2020-01-14 00:20:05 +0200

    das

--- projects/SVM/SVM_sklearn.py
+++ projects/SVM/SVM_sklearn.py
@@ -2,24 +2,28 @@ import numpy as np
 from sklearn import preprocessing, model_selection, neighbors, svm
 import pandas as pd
 
-df = pd.read_csv('breast-cancer-wisconsin.data')
+df = pd.read_csv('football_players2.txt')
 df.replace('?', -99999, inplace=True)
-df.drop(['id'], 1, inplace=True)
+df.drop(['name'], 1, inplace=True)
 
-X = np.array(df.drop(['class'], 1))
-y = np.array(df['class'])
+X = np.array(df.drop(['position'], 1))
+y = np.array(df['position'])
 
 X_train, X_test, y_train, y_test = model_selection.train_test_split(
-    X, y, test_size=0.2)
-
+    X, y, test_size=0.4)
 clf = svm.SVC()
 
 clf.fit(X_train, y_train)
-confidence = clf.score(X_test, y_test)
-print(confidence)
+accuracy = clf.score(X_test, y_test)
+print(accuracy)
 
-example_measures = np.array(
-    [[4, 2, 1, 1, 1, 2, 3, 2, 1], [4, 2, 7, 5, 5, 6, 2, 2, 1]])
-example_measures = example_measures.reshape(len(example_measures), -1)
-prediction = clf.predict(example_measures)
-print(prediction)
+example_measures = {'defender': np.array([[81, 36]]),
+                    'striker': np.array([[22, 85]])
+                    }
+
+for example in example_measures.items():
+    prediction = clf.predict(example[1])
+    print(f'Prediction of {example[0]} was: {prediction}')
+
+another_test = np.array([[11, 99]])
+print(clf.predict(another_test))
